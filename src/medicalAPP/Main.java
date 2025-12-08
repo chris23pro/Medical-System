7aac8e3f7bb939682ef45b926ba2205b833848c2
Christopher-dbz <co22aam@herts.ac.uk>
2025-12-08 20:21:13 +0000

    added a main class

---
+++ src/medicalAPP/Main.java
@@ -0,0 +1,8 @@
+package medicalAPP;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        // Add your project logic here
+    }
+}
