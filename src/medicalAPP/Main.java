544a2549d513379cd36502c92dcd3bf323090799
chris <chris@LAPTOP-F2G3I405>
2025-12-31 16:18:29 +0000

    updated main menu skeleton provided

--- src/medicalAPP/Main.java
+++
@@ -1,8 +0,0 @@
-package medicalAPP;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
-        // Add your project logic here
-    }
-}
---
+++ src/view/MainMenu.java
@@ -0,0 +1,7 @@
+package view;
+
+public class MainMenu {
+    public static void main(String[] args) {
+        // start menu here
+    }
+}
